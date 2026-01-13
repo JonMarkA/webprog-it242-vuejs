4a55ba97ae81d7e7f4700e2d01a910fccd713318
JonMarkA <jvagbulos2@student.apc.edu.ph>
2026-01-13 02:46:43 +0000

    added component food item

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Banana',
+      message: 'I like banana'
+    }
+  }
+};
+</script>
+
+<style>
+ #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  #app > div:hover {
+  cursor: pointer;
+}
+</style>
